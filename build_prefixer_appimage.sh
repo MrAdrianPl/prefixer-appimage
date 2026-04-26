6f2d05b9c204eb3fa9368ba9020ad08f4f1c59ef
MrAdrianPl <55670902+MrAdrianPl@users.noreply.github.com>
2026-04-26 19:22:55 +0200

    added appimage building script

---
+++ build_prefixer_appimage.sh
@@ -0,0 +1,35 @@
+
+set -eux
+
+ARCH="$(uname -m)"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+
+export ICON=DUMMY
+export DESKTOP=DUMMY
+export OUTPATH=./dist
+export MAIN_BIN=prefixer
+export OUTNAME=prefixer-"$ARCH".AppImage
+export DEPLOY_PYTHON=1
+
+pacman -Syu --noconfirm \
+	base-devel       \
+	curl             \
+	git              \
+	wget             \
+	xorg-server-xvfb \
+	zsync \
+	python-pip
+
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+
+pip install prefixer --break-system-packages
+
+echo "Bundling AppImage..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun /usr/bin/prefixer
+
+./quick-sharun --make-appimage
